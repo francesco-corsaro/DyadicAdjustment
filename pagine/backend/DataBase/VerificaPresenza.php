85cca7a58fc40910f9059e634ba8743d26a1ca25
francesco-corsaro <corsaro.francesco.psi@gmail.com>
2020-02-27 15:09:08 +0100

    Sistemato il programma backend per verificare la presenza nella tabella anagrafica e se presente non inserisce i dati post nel database

--- pagine/backend/DataBase/VerificaPresenza.php
+++ pagine/backend/DataBase/VerificaPresenza.php
@@ -3,7 +3,7 @@
 
 require 'ConnectDataBase.php'; //serve a connettersi al database
 
-$sql = "SELECT Email FROM Anagrafica";
+$sql = "SELECT Email FROM Anagrafica WHERE Email='".$email."'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -18,26 +18,25 @@ if ($result->num_rows > 0) {
             /* var_dump($row);*/
             
             header("location:/DyadicAdjustment/pagine/Login.php");
-        }else{
-            require 'backend/DataBase/InsertRegistrazione.php';
-            Inserisci_id(Anagrafica, $email, $nome, $cognome, $_POST[genere],$_POST[gg],$_POST[mese],$_POST[anno], $hash);
-            echo $stato.'<br> Stato query: '.$stato1.$_SESSION['errorePres'];
-            header("location: /DyadicAdjustment/pagine/Login.php");
         }
     }
     
+}else{
+    global $presente;$presente="no";
+    
+}
+$conn->close();
+
+if ($presente==="no") {
+    require 'backend/DataBase/InsertRegistrazione.php';
+    Inserisci_id(Anagrafica, $email, $nome, $cognome, $_POST[genere],$_POST[gg],$_POST[mese],$_POST[anno], $hash);
+    echo $stato.'<br> Stato query: '.$stato1.$_SESSION['errorePres'];
+     //header("location: /DyadicAdjustment/pagine/Login.php");
+   
 }
 
-  /*  }
-} elseif ($result==FALSE){
-    $_SESSION['denied']=1;
-    
-}
-else {
-    $_SESSION['denied']=1;
-    
-}*/
-$conn->close();
+ 
+
 /*$_SESSION['denied']=" 1 " ;
 header("location: /Login/Frontend/LogForm.php");*/
 ?>
