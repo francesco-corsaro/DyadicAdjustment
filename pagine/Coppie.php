9adb5048b82e9ec144305f683397bf5223b71492
francesco-corsaro <corsaro.francesco.psi@gmail.com>
2020-02-27 13:00:01 +0100

    Inserito l'input text per chiedere il nome

--- pagine/Coppie.php
+++ pagine/Coppie.php
@@ -20,9 +20,11 @@ session_start();
         Benvenuto,<br>
          il tuo codice utente è:<br> <b class="titolo"><?php echo $_SESSION['codice']; ?></b>.
         <br>Prima di poter accedere al test è necessario inserire qui sotto 
-        il codice utente del prorprio partner. 
+        il  nome e il codice utente del prorprio partner. 
         </div>
         <form name="myForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+      
+        <input name="nome" type="TEXT" placeholder="Nome Partner" required>
         <input name="cod_utente" type="number" Id="cod_utente" placeholder="Cod. partner" maxlength="6"  oninput="validateForm()" required>
         <div class="nascita">
          
